88d622550db9a70eb3d8cff9948fe04fa3161c62
Patrick Ferris <pf341@patricoferris.com>
2021-11-04 14:38:35 +0000

    Initial macOS backend work.
    Co-authored-by: Kate <kit.ty.kate@disroot.org>

--- lib/store_spec.ml
+++ lib/store_spec.ml
@@ -4,17 +4,17 @@ open Lwt.Infix
 
 type t = [
   | `Btrfs of string  (* Path *)
-  | `Zfs of string    (* Pool *)
+  | `Zfs of string    (* Path with pool at end *)
 ]
 
 let of_string s =
   match Astring.String.cut s ~sep:":" with
-  | Some ("zfs", pool) -> Ok (`Zfs pool)
+  | Some ("zfs", path) -> Ok (`Zfs path)
   | Some ("btrfs", path) -> Ok (`Btrfs path)
   | _ -> Error (`Msg "Store must start with zfs: or btrfs:")
 
 let pp f = function
-  | `Zfs pool -> Fmt.pf f "zfs:%s" pool
+  | `Zfs path -> Fmt.pf f "zfs:%s" path
   | `Btrfs path -> Fmt.pf f "btrfs:%s" path
 
 type store = Store : (module S.STORE with type t = 'a) * 'a -> store
@@ -23,6 +23,6 @@ let to_store = function
   | `Btrfs path ->
     Btrfs_store.create path >|= fun store ->
     Store ((module Btrfs_store), store)
-  | `Zfs pool ->
-    Zfs_store.create ~pool >|= fun store ->
+  | `Zfs path ->
+    Zfs_store.create ~path >|= fun store ->
     Store ((module Zfs_store), store)
